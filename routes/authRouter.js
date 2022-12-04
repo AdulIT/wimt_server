501bf8b9d1c83324fdd5855047b961dbaa9ba6b8
AdulIt <spilberg.max@bk.ru>
2022-12-04 23:26:10 +0600

    feat: google authorization

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/', (req, res) => {
-    res.send('auth');
-})
+
+router.get(
+    '/google',
+    passport.authenticate('google', {
+        scope: 'profile',
+    })
+);
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {session: true}), (_, res) => {
+        res.redirect(`${process.env.CLIENT_URL}`);
+    }
+);
 
 module.exports = router;
